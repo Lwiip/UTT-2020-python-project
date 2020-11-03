a9eb1797dcba7f5801eb99d3c76d8e83bc2b0774
“Lwiip” <louisgalava.net@gmail.com>
2020-11-03 18:57:50 +0100

    create functions and add selection option

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import pandas as pd 
+import filtering
 
+##########################
+#Import file and get headers from the file
+##########################
 #Import data with specific delimiter as a panda dataframe
 dfdata = pd.read_csv("sites-agriculture-urbaine.csv",sep=';') 
-
 #Get list of headers from the file imported
 headers = list(dfdata.columns) 
 
-#Create dictionnary, key is the collumns of the file, value is the regex pattern
+
+##########################
+#Get info from user
+##########################
+#Create dictionnary, key is the columns of the file, value is the regex pattern and boolean value (print or not print)
 dfilter = {}
 for header in headers:
-    dfilter["cfilter_{0}".format(header)] = '.*'
-
+    dfilter["cregex_{0}".format(header)] = '.*'
+    dfilter["cselect_{0}".format(header)] = True
 #Random regex pattern, this will come from the user
-dfilter['cfilter_nom']='quartier'
-dfilter['cfilter_lieu']='Louis'
-dfilter['cfilter_contact']='^iss.*'
+dfilter['cregex_nom']='quartier'
+dfilter['cregex_lieu']='Louis'
+dfilter['cregex_contact']='^iss.*'
+#Random select, this will come from the user
+dfilter['cselect_nom']=False
 
 
-#Change all the types of the dataframe to str, needed for apply contains function
-dfdatacopy = dfdata.applymap(str)
-#print (dfdatacopy.dtypes)
+##########################
+#Apply inputs from user
+##########################
+#Apply selection
+dfdataworking = dfdata.copy()
+dfdataworking = filtering.applyselection(dfdataworking, dfilter)
+
+#Apply regex
+dfdataregex = filtering.applyregex(dfdataworking, dfilter)
+print(dfdataregex)
 
 
-for header in headers:
-    pattern = str(dfilter["cfilter_{0}".format(header)])
-    print("=>>>>>>>>>>>>>>>>>>>>>>> ",pattern)
-    dfdatacopy = dfdatacopy[dfdatacopy[header].str.contains(pattern)==True]
-print(dfdatacopy)    
-
-
-
+##########################
+#Export to a file
+##########################
 
 
 
