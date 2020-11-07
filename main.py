943ec79e6fed70bacd12f7ce0917ada82460afa1
“Lwiip” <louisgalava.net@gmail.com>
2020-11-07 12:09:10 +0100

    add filtering, date filtering is nok

--- main.py
+++ main.py
@@ -13,29 +13,48 @@ headers = list(dfdata.columns)
 ##########################
 #Get info from user
 ##########################
-#Create dictionnary, key is the columns of the file, value is the regex pattern and boolean value (print or not print)
+#Array sorting
+asort = []
+asort = ['nom','lieu','cp']
+
+#Create dictionnary, key is the columns of the file, value is the regex pattern, boolean value (print or not print), boolean value (ascending, descnedning or None sorting)
 dfilter = {}
 for header in headers:
     dfilter["cregex_{0}".format(header)] = '.*'
     dfilter["cselect_{0}".format(header)] = True
+    dfilter["csort_{0}".format(header)] = None
 #Random regex pattern, this will come from the user
 dfilter['cregex_nom']='quartier'
 dfilter['cregex_contact']='^iss.*'
 #Random select, this will come from the user
-dfilter['cselect_nom']=True
+dfilter['cselect_nom']=False
+#Random sort, this will ccome from the user
+dfilter['csort_nom']=False
+dfilter['csort_lieu']=True
+dfilter['csort_cp']=True
+
 
 
 ##########################
 #Apply inputs from user
 ##########################
-#Apply selection
 dfdataworking = dfdata.copy()
+print(dfdataworking)
+print("-------------------------------")
+
+#Apply sorting
+dfdataworking = filtering.applysort(dfdataworking, asort, dfilter)
+print(dfdataworking)
+print("-------------------------------")
+
+#Apply selection
 dfdataworking = filtering.applyselection(dfdataworking, dfilter)
+print(dfdataworking)
+print("-------------------------------")
 
 #Apply regex
 dfdataregex = filtering.applyregex(dfdataworking, dfilter)
 print(dfdataregex)
-
 
 ##########################
 #Export to a file
