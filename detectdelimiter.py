9572b6027474fc717e6b125af40990c213633f7a
Lwiip <louisgalava.net@gmail.com>
2020-11-28 12:58:00 +0100

    add auto detect delimiter

---
+++ detectdelimiter.py
@@ -0,0 +1,61 @@
+import operator
+import argparse
+
+def computeoccurence(lignes, ddelimiters):
+    """
+    Adds to the dictionary (dico) the number of each predefined delimiter for the first 5 lines. 
+
+    :param lignes: Recovery of the first 5 lines of the file 
+    :param ddelimiters: Retrieves the list of defined delimiters 
+    """
+
+    #init
+    dico = {}
+    for key, value in ddelimiters.items():
+        dico["{0}".format(key)] = []
+
+    #compute
+    for ligne in lignes:
+        # Add to dico : 'virgule': [0, 1, 1, 1, 1], 'ptvirgule': [8, 8, 8, 8, 8] ....
+        for key, value in ddelimiters.items():  
+            dico["{0}".format(key)].append(ligne.count(value))  
+
+    return dico
+
+
+def getdelimitername(dico):
+    """
+    Retrieves the dictionary (dico) and count the number of occurrences of the predefined delimiters  
+
+    """
+
+    dshortlist = {}
+
+    for key, values in dico.items():
+        # A set is a collection that only keeps one copy of any object. So set([1,1,1,1,1,1]) is the same as set([1])
+        # len(set(values)) == 1 check if all values are the same
+        if len(set(values)) == 1:
+            dshortlist[key] = values[0]
+
+    return max(dshortlist.items(), key=operator.itemgetter(1))[0]
+
+
+def getautodelimiter(filename, ddelimiters):
+    """
+    Automatic delimiter detection
+
+    :param filename: Input file 
+    :param ddelimiters: List of predefined delimiters
+    """
+
+    # Open the file csv or txt
+    with open(filename, 'r',encoding="utf-8") as file:
+        #entete = f.readline().rstrip('\n').split(",")
+        lignes = file.readlines()[0:5]
+        dico2 = computeoccurence(lignes, ddelimiters)
+   
+    delimitername = getdelimitername(dico2)
+    finaldelimiter = ddelimiters[delimitername]
+
+    return finaldelimiter
+
