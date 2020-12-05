beea17b791ab0b92c6cf7425fda2b4e3abf16fbf
Lwiip <louisgalava.net@gmail.com>
2020-12-05 13:27:09 +0100

    recupère les données des checkbox

--- testLouis.py
+++ testLouis.py
@@ -5,43 +5,67 @@
 
 
 from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 import pandas as pd
 
-def inputsGraphic(pd):
-    headers = []
-    headers = list(pd.columns)
 
-    totalcolumn = len(headers)
-    relativesize = 1 / totalcolumn
-    size = round((1920/totalcolumn)/9)
-    pos = 0
-    count = 0
+def getinputsGraphic(dfilterb, asort, headers):
     
-    for column in headers:
+    print("coucou")
+
+    dfilter = {}
+    for header in headers:
+        #get checkbox and save it in dfilter
+        dfilter["cselect_{0}".format(header)] = dfilterb["cselect_{0}".format(header)].get()
+
+    print(dfilter)
+
+
+def setinputsGraphic(headers):
+
+    #init neede for graphic interface 
+    totalcolumn = len(headers)
+    size = round((1920/totalcolumn)/9)
+    count = 0
+
+    #init same model as dfilter command line but for button
+    dfilterb = {}
+    asort = []
+    
+    for header in headers:
+        #grid configuration (take all the space and add a little of padding)
         inputsFrame.columnconfigure(count, weight=1,pad=0.1)
 
-        label1 = Label(inputsFrame,text=column, width=size)
-        label1['font']=('Arial', 8)
-        label1.grid(row=1, column=count)
+        #label to print column name
+        label = Label(inputsFrame,text=header, width=size)
+        label['font']=('Arial', 8)
+        label.grid(row=1, column=count)
 
-        check = Checkbutton(inputsFrame, text='select/deselect', width=size)
-        check.select()
+        #checkbutton selection
+        dfilterb["cselect_{0}".format(header)] = tk.BooleanVar() 
+        check = Checkbutton(inputsFrame, text='delete column', width=size, var=dfilterb["cselect_{0}".format(header)])
         check.grid(row=2, column=count)
 
+        #regex inputs
         value = StringVar() 
         value.set(".*")
         entree = Entry(inputsFrame, textvariable=value,width=size)
         entree.grid(row=3, column=count)
 
+        #sort inputs
         bouton = Button(inputsFrame,text='sort',width=size)
         bouton.grid(row=4, column=count)
 
-        pos = pos + relativesize 
         count = count + 1 
 
+    #button compute
+    buttonCompute = Button(commandFrame, text="Compute", command = lambda: getinputsGraphic(dfilterb, asort, headers))
+    buttonCompute.place(relx=0.2, rely=0.1)
 
+    return dfilterb, asort
+    
 
 #function to show the table
 def showTable(pd):
@@ -71,6 +95,8 @@ def OpenFile():
     try:
         with open(log,"r") as UseFile:
             data = pd.read_csv(UseFile, header=0, sep=';')
+            headers = []
+            headers = list(data.columns)
             
 
             #Clean widget, needed if you open another file
@@ -80,15 +106,21 @@ def OpenFile():
                 widget.destroy()
             
             #setinputs function
-            inputsGraphic(data)
+            dfilter, asort = setinputsGraphic(headers)
             #showTable function
             showTable(data)
-
+            #getinputs
+            #getinputsGraphic(dfilter, asort, headers)
+        
     #Exception handling
     except FileNotFoundError:
         messagebox.showerror("No file exists")
     except:
         messagebox.showerror("Error")
+
+        
+
+    
   
 
 #main window     
@@ -102,6 +134,7 @@ commandFrame = ttk.LabelFrame(fenetre, text="Commande")
 commandFrame.place(relx=0, rely=0.02, height=50, width=1920)
 buttonFile = Button(commandFrame, text="File", command = OpenFile)
 buttonFile.place(relx=0, rely=0.1)
+
 
 #Inputs Panel
 inputsFrame = ttk.LabelFrame(fenetre, text="Inputs")
